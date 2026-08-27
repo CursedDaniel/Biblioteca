3bacdc1f4db0539e60caf10ec3bafe328f50a6cf
CursedDaniel <cetroloco.daniel@gmail.com>
2026-08-26 23:31:26 -0500

    feat: crear entidad usuario

---
+++ usuario.py
@@ -0,0 +1,20 @@
+class Usuario:
+    def __init__(
+        self,
+        id_usuario: int,
+        nombre: str,
+        apellido: str,
+        documento: str,
+        correo: str,
+        telefono: str,
+        fecha_registro: str,
+        estado: str,
+    ):
+        self.id_usuario = id_usuario
+        self.nombre = nombre
+        self.apellido = apellido
+        self.documento = documento
+        self.correo = correo
+        self.telefono = telefono
+        self.fecha_registro = fecha_registro
+        self.estado = estado
