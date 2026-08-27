56d9d090177f9818a1bdc9548168ec9ea383f46b
CursedDaniel <cetroloco.daniel@gmail.com>
2026-08-27 01:12:08 -0500

    Se añade UUID se optimiza formato y atributos

--- src/entities/libro.py
+++ src/entities/libro.py
@@ -1,10 +1,10 @@
+import uuid
 from datetime import date
 
 
 class Libro:
     def __init__(
         self,
-        id_libro: int,
         titulo: str,
         fecha_publicacion: date,
         numero_paginas: str,
@@ -12,8 +12,9 @@ class Libro:
         descripcion: str,
         id_categoria: str,
         id_editorial: str,
+        id_libro: uuid.UUID | None = None,
     ):
-        self.id_libro = id_libro
+        self.id_libro = id_libro if id_libro is not None else uuid.uuid4()
         self.titulo = titulo
         self.fecha_publicacion = fecha_publicacion
         self.numero_paginas = numero_paginas
@@ -21,3 +22,15 @@ class Libro:
         self.descripcion = descripcion
         self.id_categoria = id_categoria
         self.id_editorial = id_editorial
+
+    def __str__(self) -> str:
+        return (
+            f"ID: {self.id_libro}\n"
+            f"Título: {self.titulo}\n"
+            f"Fecha de Publicación: {self.fecha_publicacion}\n"
+            f"Número de Páginas: {self.numero_paginas}\n"
+            f"Idioma: {self.idioma}\n"
+            f"Descripción: {self.descripcion}\n"
+            f"ID Categoría: {self.id_categoria}\n"
+            f"ID Editorial: {self.id_editorial}"
+        )
