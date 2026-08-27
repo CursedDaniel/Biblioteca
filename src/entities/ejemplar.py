df57d67ead5e29decdfe8ab4dea136e313c0a279
CursedDaniel <cetroloco.daniel@gmail.com>
2026-08-27 00:33:13 -0500

    Se Actualizan Librerias y se importa uuid

--- src/entities/ejemplar.py
+++ src/entities/ejemplar.py
@@ -1,19 +1,20 @@
+import uuid
 from datetime import date
 
 
 class Ejemplar:
     def __init__(
         self,
-        id_ejemplar: int,
-        id_libro: int,
+        id_libro: uuid.UUID,
         codigo_inventario: str,
         fecha_adquisicion: date,
         estado: str,
         ubicacion: str,
+        id_ejemplar: uuid.UUID | None = None,
     ):
-        self.id_ejemplar = id_ejemplar
+        self.id_ejemplar = id_ejemplar if id_ejemplar is not None else uuid.uuid4()
         self.id_libro = id_libro
-        self.codigo_inventario = codigo_inventario
+        self.codigo_inventario = codigo_inventario.strip()
         self.fecha_adquisicion = fecha_adquisicion
-        self.estado = estado
-        self.ubicacion = ubicacion
+        self.estado = estado.strip()
+        self.ubicacion = ubicacion.strip()
