c439a3624f8067455b56222d93d549c1a883c560
EstebanTriana21 <esteban212518@gmail.com>
2026-08-28 17:28:57 -0500

    feat: se realiza la entidad categoria

---
+++ src/entities/categoria.py
@@ -0,0 +1,21 @@
+# se hace la entidad categoria para poder crear la tabla en la base de datos
+import uuid
+from datatime import date
+
+class Categoria:
+    def __init__(
+        self,
+        nombre: str,
+        descripcion: str,
+        id_categoria: uuid.UUID | None = None,
+    ):
+        self.id_categoria = id_categoria if id_categoria is not None else uuid.uuid4()
+        self.nombre = nombre.strip()
+        self.descripcion = descripcion.strip()
+
+    def __str__(self) -> str:
+        return (
+            f"ID: {self.id_categoria}\n"
+            f"Nombre: {self.nombre}\n"
+            f"Descripción: {self.descripcion}"
+        )
