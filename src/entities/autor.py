de071b027332d85eb1f6dfb9afcd4b420f76776a
CursedDaniel <cetroloco.daniel@gmail.com>
2026-08-28 01:42:00 -0500

    Se corrigen Conflictos con la Mayuscula

--- src/entities/autor.py
+++ src/entities/autor.py
@@ -1,9 +1,8 @@
-# comienzo entidad autor
-
 import uuid
 from datetime import date
 
-class autor:
+
+class Autor:
     def __init__(
         self,
         nombre: str,
@@ -13,14 +12,14 @@ class autor:
         biografia: str,
         id_autor: uuid.UUID | None = None,
     ):
-        
+
         self.id_autor = id_autor if id_autor is not None else uuid.uuid4()
         self.nombre = nombre.strip()
         self.apellido = apellido.strip()
         self.fecha_nacimiento = fecha_nacimiento
         self.nacionalidad = nacionalidad.strip()
         self.biografia = biografia.strip()
-        
+
     def __str__(self) -> str:
         return (
             f"ID: {self.id_autor}\n"
