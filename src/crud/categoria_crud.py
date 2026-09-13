38baafcb02bf52ce140d3ed5a11adb2f81e8ca43
CursedDaniel <cetroloco.daniel@gmail.com>
2026-09-12 23:15:34 -0500

    Fix: Se corrige categoria

--- src/crud/categoria_crud.py
+++ src/crud/categoria_crud.py
@@ -1,19 +1,14 @@
 import uuid
-
 from sqlalchemy.orm import Session
-
 from src.entities.categoria import Categoria
 
 
 class CategoriaCrud:
+
     def __init__(self, session: Session):
         self.session = session
 
-    def crear(
-        self,
-        nombre: str,
-        descripcion: str,
-    ) -> Categoria:
+    def crear(self, nombre: str, descripcion: str) -> Categoria:
         categoria = Categoria(
             nombre=nombre.strip(),
             descripcion=descripcion.strip(),
@@ -25,18 +20,11 @@ class CategoriaCrud:
 
         return categoria
 
-    def obtener_por_id(
-        self,
-        id_categoria: uuid.UUID,
-    ) -> Categoria | None:
+    def obtener_por_id(self, id_categoria: uuid.UUID) -> Categoria | None:
         return self.session.get(Categoria, id_categoria)
 
-    def obtener_por_nombre(
-        self,
-        nombre: str,
-    ) -> Categoria | None:
+    def obtener_por_nombre(self, nombre: str) -> Categoria | None:
         nombre_normalizado = nombre.strip().lower()
-
         categorias = self.session.query(Categoria).all()
 
         for categoria in categorias:
