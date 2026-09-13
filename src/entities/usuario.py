b3f4aae422f2142af8e065b9759e5cc19b875ade
CursedDaniel <cetroloco.daniel@gmail.com>
2026-09-12 20:16:25 -0500

    fix: se agregan relaciones en usuario.py a SQLachemy

--- src/entities/usuario.py
+++ src/entities/usuario.py
@@ -3,6 +3,7 @@ from datetime import date
 
 from sqlalchemy import Column, Date, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from src.database.database import Base
 
@@ -13,12 +14,20 @@ class Usuario(Base):
     id_usuario = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
     nombre = Column(String(100), nullable=False)
+
     apellido = Column(String(100), nullable=False)
+
     documento = Column(String(30), nullable=False, unique=True)
+
     correo = Column(String(150), nullable=False, unique=True)
+
     telefono = Column(String(30), nullable=False)
+
     fecha_registro = Column(Date, nullable=False, default=date.today)
+
     estado = Column(String(20), nullable=False, default="activo")
+
+    prestamos = relationship("Prestamo", back_populates="usuario")
 
     def __str__(self) -> str:
         return (
